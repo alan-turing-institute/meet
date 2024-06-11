f3059d21780cbb83df7a6c212daa3166dd43deac
Penelope Yong <penelopeysm@gmail.com>
2024-06-11 10:05:59 +0100

    minimal Azure stuff

--- app/Azure.hs
+++ app/Azure.hs
@@ -1,31 +1,30 @@
-module Azure (getPerson) where
+{-# LANGUAGE DeriveGeneric #-}
 
+module Azure (getPerson, getToken) where
+
+import Data.Aeson
+import Data.Aeson.Encoding (encodingToLazyByteString)
+import Data.Text (Text)
+import GHC.Generics
 import Network.HTTP.Req
 import Types
 
 getPerson :: String -> IO (Maybe Person)
 getPerson = undefined
 
-newtype Token = Token {unToken :: String}
+newtype Token = Token {unToken :: Text}
 
--- | rum
-clientId :: String
-clientId = "a462354f-fd23-4fdf-94f5-5cce5a6c27c7"
+rumClientId :: Text
+rumClientId = "a462354f-fd23-4fdf-94f5-5cce5a6c27c7"
 
-tenantId :: String
-tenantId = "4395f4a7-e455-4f95-8a9f-1fbaef6384f9"
-
-data DeviceCodeBody = DeviceCodeBody
-  { clientId :: String,
-    scope :: String
-  }
+rumTenantId :: Text
+rumTenantId = "4395f4a7-e455-4f95-8a9f-1fbaef6384f9"
 
 getToken :: IO (Either String Token)
 getToken = do
-  let deviceCodeUrl = "https://login.microsoftonline.com/" ++ tenantId ++ "/oauth2/v2.0/devicecode"
-  let postBody = DeviceCodeBody clientId "user.read calendars.read.shared"
-  respBody <- runReq defaultHttpConfig $ do
-    v <- req POST (https "login.microsoftonline.com" /: tenantId /: "oauth2" /: "v2.0" /: "devicecode") (ReqBodyJson postBody) jsonResponse mempty
-    pure (responseBody v)
+  resp <- runReq defaultHttpConfig $ do
+    let url = https "login.microsoftonline.com" /: rumTenantId /: "oauth2" /: "v2.0" /: "devicecode"
+    req POST url (ReqBodyUrlEnc ("client_id" =: rumClientId <> "scope" =: ("user.read calendars.read.shared" :: Text))) jsonResponse mempty
+  print (responseBody resp :: Value)
 
-  print respBody
+  pure $ Left "oops"
