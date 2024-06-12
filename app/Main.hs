ab656435a55069b3b62cd5c40413d69d2d7c07b7
Penelope Yong <penelopeysm@gmail.com>
2024-06-12 15:45:08 +0100

    Fix errors

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,10 @@ module Main where
 
 import Args
 import Azure
-import Data.Text (Text)
-import qualified Data.Text as T
 import Data.Time.Clock (addUTCTime, getCurrentTime, secondsToNominalDiffTime)
 import Data.Time.LocalTime (LocalTime (..))
 import Meetings (findRelativeMeetings)
 import System.Exit (exitFailure)
-import System.IO (hFlush, isEOF, stdout)
 import Types (toPerson)
 import Utils
 
@@ -20,13 +17,9 @@ main = do
       meetingDuration = duration args
       searchStartDate = startDate args
       searchSpan = timespan args
-
   startDate' <- case searchStartDate of
     Just d -> pure d
     Nothing -> localDay <$> getCurrentLondonTime
-
-  print startDate'
-  error "don't continue"
 
   chunks <- gracefulDivide meetingDuration meetingInterval -- exits early if duration < interval
   eitherToken <- getToken
