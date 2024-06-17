74f139d6c5fb03a0c5d109e9d256e2728a364b32
Nathan Simpson <nsimpson@turing.ac.uk>
2024-06-17 16:20:53 +0100

    initial attempt in main, will refactor if working

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,62 @@
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Main where
 
 import Args (Args (..), getArgs)
 import Azure (fetchSchedules, getToken)
+import Control.Applicative
 import Control.Monad (when)
+import Data.ByteString (ByteString)
 import qualified Data.List.NonEmpty as NE
+import Data.Text (Text, pack)
 import qualified Data.Text.IO as T
 import Data.Time.Calendar (addDays)
 import Data.Time.LocalTime (LocalTime (..), TimeOfDay (..), getCurrentTimeZone, localTimeToUTC)
-import Entities (Days (..), Room (..), allRooms)
+import Data.Yaml (FromJSON (..), (.:))
+import qualified Data.Yaml as Y
+import Entities (Days (..), Person (..), Room (..), allRooms)
 import Meetings (chooseBestMeeting, getMeetings)
 import Print (infoPrint, prettyPrint)
 import System.Exit (exitSuccess)
+import Text.RawString.QQ
 import Utils
+import Prelude -- Ensure Applicative is in scope and we have no warnings, before/after AMP.
 
+-- to refactor ---------------------------------------------------------------
+data Group = Group
+  { name :: Text,
+    emails :: [Text]
+  }
+  deriving (Eq, Show)
+
+instance FromJSON Group where
+  parseJSON = Y.withObject "Group" $ \v ->
+    Group
+      <$> v .: "name"
+      <*> v .: "emails"
+
+newtype Config = Config
+  { groups :: [Group]
+  }
+  deriving (Eq, Show)
+
+instance FromJSON Config where
+  parseJSON = Y.withObject "Config" $ \v ->
+    Config <$> v .: "groups"
+
+-- | Example of a YAML configuration file for groups of email addresses.
+configYaml :: ByteString
+configYaml =
+  [r|
+groups:
+    - name: Friends
+    emails:
+        - a@turing.ac.uk
+        - b@example.com
+|]
+
+--------------------------------------------------------------------------------
 main :: IO ()
 main = do
   args <- getArgs
@@ -41,13 +83,21 @@ main = do
     T.putStrLn "Perhaps try reducing the number of people who need to be in-person?"
     exitSuccess
 
-  token <- getToken
-  (personSchs, roomSchs) <- fetchSchedules token ppl okRooms startTime' endTime' intervalMinutes
-  let goodMeetings = getMeetings personSchs roomSchs inPerson nChunks startTime' intervalMinutes localTz
+  case Y.decodeEither' @Config configYaml of
+    -- Handle the case where the config file is not valid YAML explicitly,
+    -- and tell the compiler it is definitely a Config.
+    -- (this seemed to be needed for the typechecker to be happy)
+    Left e -> T.putStrLn $ "Error parsing config: " <> pack (show e)
+    Right config -> do
+      let groupEmails = concatMap emails $ groups config
+      token <- getToken
+      let ppl' = ppl ++ map Person groupEmails
+      (personSchs, roomSchs) <- fetchSchedules token ppl' okRooms startTime' endTime' intervalMinutes
+      let goodMeetings = getMeetings personSchs roomSchs inPerson nChunks startTime' intervalMinutes localTz
 
-  case NE.nonEmpty goodMeetings of
-    Nothing -> T.putStrLn "No meetings were available. :("
-    Just ms ->
-      if argsFeelingLucky args
-        then infoPrint (chooseBestMeeting ms) inPerson
-        else prettyPrint goodMeetings
+      case NE.nonEmpty goodMeetings of
+        Nothing -> T.putStrLn "No meetings were available. :("
+        Just ms ->
+          if argsFeelingLucky args
+            then infoPrint (chooseBestMeeting ms) inPerson
+            else prettyPrint goodMeetings
